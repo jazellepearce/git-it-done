9e600cc9b16a3d7b591ef2ee58abdf33b064e9fe
Jazelle Pearce <pjazelle@yahoo.com>
2022-04-19 07:59:28 -0500

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,23 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+
+    repoNameEl.textContent = repoName;
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }else {
+        document.location.replace("./index.html")
+    }
+}
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -20,8 +37,8 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+    // if not successflul, redirect to homepage
+    document.location.replace("./index.html")
     }
   });
 };
